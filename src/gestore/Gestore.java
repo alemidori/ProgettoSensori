44e3bd0134f030fabb5288bd96c3cfeb797fb348
Alessandra Pascazio <alemidori777@gmail.com>
2015-06-08 17:16:18 +0200

    reteSens FINAL

--- src/gestore/Gestore.java
+++ src/gestore/Gestore.java
@@ -12,20 +12,22 @@ import java.util.ArrayList;
  * Created by Alessandra on 04/06/15.
  */
 public class Gestore {
+    private static Gson gson = new Gson();
     public static void main(String args[]) throws Exception {
         ServerSocket serverSocket = new ServerSocket(5555);
+
         while (true) {
             Socket socket = serverSocket.accept();
-//            Gson gson = new Gson();
-//            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            String res = br.readLine();
-//            Object what = gson.fromJson(res, Object.class);
-//            if(what instanceof ArrayList){
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String res = br.readLine();
+            Object what = gson.fromJson(res, Object.class);
+            if(what instanceof ArrayList){
                 new Thread(new ReadMisThread(socket)).start();
-//            }
-//            else{
-//                new Thread(new ReadMsgThread(socket)).start();
-//            }
+            }
+            else{
+                new Thread(new ReadMsgThread(socket)).start();
+            }
 
 
         }
